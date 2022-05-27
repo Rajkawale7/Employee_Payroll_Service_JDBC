89ceef8d5f5c976e21357b8a6fcc8d668147125c
Rajkawale7 <Rajkawale712@gmail.com>
2022-05-27 09:14:36 +0530

    Added: Welcome to the program

---
+++ src/main/java/com/bridgelabz/EmployeePyrollService.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class EmployeePyrollService {
+    public static void main(String[] args) throws ClassNotFoundException {
+        System.out.println("*****WELCOME TO THE PROGRAM ON JAVA DATABASE CONNECTIVITY*****");
+    }
+}
